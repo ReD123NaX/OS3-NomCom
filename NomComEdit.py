bf86915b4f97654bac6035afa2033a55aea0a0bd
ReD123NaX <xander.lammertink@os3.nl>
2015-10-02 00:28:32 +0200

    Finished functions, starting procedure

--- NomComEdit.py
+++ NomComEdit.py
@@ -1,73 +1,120 @@
-#!/usr/bin/python
+# !/usr/bin/python
+__author__ = 'Xander Lammertink'
 
 # Import libraries
 import hashlib
 import struct
 
-# Ask for candidates and put them into a list
-print "\nPlease enter the names of the candidates in alphabetic order."
-print "Please be aware that the names should be separated by ', '.\n"
 
-candidates = raw_input("Names: ")
-candidates = candidates.split(', ')
+def askCandidates(listname):
+    # Ask for candidates and put them into a list
+    print "\nPlease enter the names of the " + listname + " in alphabetic order."
+    print "Please be aware that the names should be separated by ', '.\n"
 
-# Determine the size of the pool
-pool = len(candidates)
+    names = raw_input("Names: ")
+    names = names.split(', ')
 
-# Ask for key sources
-print "\nPlease enter the numbers of the keysources."
-print "Please be aware that numbers should be separated by a space."
-print "Different key sources should be separated by ', '\n"
+    return names
+    # Determine the size of the pool
+    # pool = len(candidates)
 
-keysource = raw_input("Key sources: ")
-keysource = keysource.split(', ')
 
-# Format the sources into a key string
-for i in range(0, len(keysource) ):
-	# Create temp list with all values
-	temp_key = str(keysource[i]).split(' ')
-	# Convert values to int
-	temp_key = map(int, temp_key)
-	# Sort values from small to large
-	temp_key.sort()
-	# Convert back to string
-	temp_key = map(str, temp_key)
-	# Put them into the original keysource[]
-	keysource[i] = " ".join(temp_key)
-	# Add '.' between numbers
-	keysource[i] = keysource[i].replace(' ', '.') + '.'
-# Add '/' after every source
-keysource = '/'.join(keysource) + '/'
+def askSplitSize(rooms):
+    splitsize = []
+    for i in range(0, rooms):
+        splitsize.append(raw_input("How many candidates should be in room " + str(i) + "? "))
+    return splitsize
 
-# Ask for the amount of output values
 
-to_select = input("\nHow many names would you like to select? ")
-print to_select * to_select
+def askKeySource():
+    # Ask for key sources
+    print "\nPlease enter the numbers of the keysources."
+    print "Please be aware that numbers should be separated by a space."
+    print "Different key sources should be separated by ', '\n"
 
-####
-#value = "9319./2.5.8.10.12./9.18.26.34.41.45./"
-#value = value.encode(encoding='ascii')
-#print value
-#print "\n\n\n"
-####
+    keysource = raw_input("Key sources: ")
+    keysource = keysource.split(', ')
 
-# Create variables for function
-result = []
+    # Format the sources into a key string
+    for i in range(0, len(keysource)):
+        # Create temp list with all values
+        temp_key = str(keysource[i]).split(' ')
+        # Convert values to int
+        temp_key = map(int, temp_key)
+        # Sort values from small to large
+        temp_key.sort()
+        # Convert back to string
+        temp_key = map(str, temp_key)
+        # Put them into the original keysource[]
+        keysource[i] = " ".join(temp_key)
+        # Add '.' between numbers
+        keysource[i] = keysource[i].replace(' ', '.') + '.'
 
-# Execute function
-for i in range (0, to_select - 1):
-    m = hashlib.md5()
-    pre = struct.pack('>H', i)
-    m.update(pre)
-    m.update(keysource)
-    m.update(pre)
+    # Add '/' after every source
+    keysource = '/'.join(keysource) + '/'
+    return keysource
 
-    hex = m.hexdigest()
-    inter = int("0x"+hex, 0)
-    print inter
-    selected = inter % (pool - i)
-    print selected
-    result.append(candidates[selected])    
 
-# Output result
-print result
+def executeNomCon(candidates, keysource, pool):
+    # candidates: The names you would like to select in alphabetical order
+    # keysource: The keysource you would like to use
+    # pool: Amount of values you would like to select
+
+    # Create variables for function
+    result = []
+
+    # Execute function
+    for i in range(0, pool):
+        m = hashlib.md5()
+        pre = struct.pack('>H', i)
+        m.update(pre)
+        m.update(str(keysource))
+        m.update(pre)
+
+        hex = m.hexdigest()
+        inter = int("0x" + hex, 0)
+        selected = inter % (pool - i)
+        result.append(candidates[selected])
+
+    # Output result
+    print result
+
+
+##
+# Varables
+##
+candidates = []
+splitsize = []
+
+# Define how many classrooms
+classrooms = raw_input("How many classrooms are available: ")
+
+##
+# Create three pre-defined lists with candidates
+##
+
+# List 0: New Full-Time Students
+candidates.append(askCandidates("new full-time students"))
+splitsize.append(askSplitSize(int(classrooms)))
+
+# List 1: New Part-Time Students
+candidates.append(askCandidates("new part-time students"))
+splitsize.append(askSplitSize(int(classrooms)))
+
+# List 2: Returning Students
+candidates.append(askCandidates("returning students"))
+splitsize.append(askSplitSize(int(classrooms)))
+
+print candidates
+print splitsize
+
+##
+# Execute NomCom procedure over the pre-defined lists
+##
+
+# Ask for the keysource
+keysource = askKeySource()
+
+# Execute NomCom procedure
+for i in range(0, len(candidates)):
+    candidates[i] = executeNomCon(candidates[i], keysource, len(candidates[i]))
